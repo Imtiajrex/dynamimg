4e09f6c14cb0c5095bdf330dc2f936315d01ed56
imtiaj <imtiajrex@gmail.com>
2023-01-16 22:16:00 +0600

    added tailwind, daisyui and dashboard page

---
+++ src/lib/components/header/non-editor-right.svelte
@@ -0,0 +1,18 @@
+<script>
+	import { Bell, Help } from 'tabler-icons-svelte';
+
+	export let menuLinkClass = '';
+</script>
+
+<!-- svelte-ignore a11y-missing-attribute -->
+<div class="flex items-center justify-end  gap-3">
+	<a class={menuLinkClass}>
+		<Help />
+	</a>
+	<a class={menuLinkClass}>
+		<Bell />
+	</a>
+	<a class="cursor-pointer outline-4 outline-primary">
+		<img src="https://i.pravatar.cc/50" class="w-10 h-10 rounded-full" />
+	</a>
+</div>
---
+++ src/routes/(dashboard)/+layout.svelte
@@ -0,0 +1,9 @@
+<script>
+	import Header from '$lib/components/header/header.svelte';
+	import '../../app.css';
+</script>
+
+<div class=" max-w-7xl p-5 w-full mx-auto min-h-screen text-black bg-bg">
+	<Header />
+	<slot />
+</div>
