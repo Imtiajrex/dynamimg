b7d24fac330eb2697a1e0e3e21a052adc7f49225
Imtiaj <imtiajrex@gmail.com>
2023-02-04 19:36:49 +0600

    fixed adapter build fails

--- src/lib/components/editor/element.svelte
+++ src/lib/components/editor/element.svelte
@@ -176,7 +176,7 @@
 				{@html element.content}
 			{:else}
 				<trix-editor
-					style={`width:100%;height:100%;position:absolute;top:0;left:0;${
+					style={`width:100%;height:100%;position:absolute;top:0;left:0;padding:0;${
 						edit ? '' : 'display: none;'
 					}`}
 					toolbar="toolbar"
